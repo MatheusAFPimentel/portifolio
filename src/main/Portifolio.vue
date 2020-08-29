806fd3a4ea63bb0e932ebc6bed7249893f6117f2
MatheusAFPimentel <matheus.algustt@gmail.com>
2020-08-29 13:59:14 -0300

    Atualização

--- src/main/Portifolio.vue
+++ src/main/Portifolio.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="Title">
     <h1>Olá, sou Matheus.</h1>
-    <h2>Front-end developer.</h2>
+    <h2>Estudante de Sistemas de Informação, front-end developer.</h2>
     <p>
       5.º período de Sistemas de informação pela UNINASSAU e graduado em Ciências Contábeis pela Faculdade Santa Helena.
       Sou apaixonado por tecnologia e a contabilidade foi o pontapé para conseguir arrecadar fundos e investir na minha carreira em tecnologia que é meu principal objetivo. Alguns dos meus conhecimentos: Java, HTML, CSS, JavaScript, TypeScript, SQL, MongoDB, React, VueJS, Angular.
